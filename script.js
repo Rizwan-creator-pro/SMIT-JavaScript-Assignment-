00e576f0157ee2c6d987b7f6335904477ffd3ef6
Muhammad Rizwan Ahmad <rimalik162534@gmail.com>
2024-03-17 23:10:33 +0500

    JavaScript-Assignment

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
-let username = "Muhammad Rizwan Ahmad"
 // Alert
 document.getElementById("alert").onclick = function () {
+    let username = 'Muhammad Rizwan Ahmad'
     alert(username)
+    let Original = " let username = 'Muhammad Rizwan Ahmad' <br>  alert(username)"
+    document.getElementById("Original").innerHTML = Original;
+    document.getElementById("Output").innerHTML = username
+
+
 }
 // Alert number
 document.getElementById("alert-num").onclick = function () {
     let alertnumber = 88888888888
     alert(alertnumber)
+    let Original = "let alertnumber = 88888888888 <br> alert(alertnumber) "
+    document.getElementById("Original").innerHTML = Original;
+    document.getElementById("Output").innerHTML = alertnumber;
 }
 // Variable name
 document.getElementById("Variable-Names").onclick = function () {
-    document.getElementById("Original").innerText = "Variable Name"
-    document.getElementById("Output").innerText = "varName"
+    document.getElementById("Original").innerHTML = "Variable Name"
+
+    let html = "<ul><li>A variable name can't contain any spaces.</li><li>A variable name can contain only letters, numbers, dollar signs, and underscores</li><li>Though a variable name can't be any of JavaScript's keywords, it can contain keywords. For example, <code>userAlert</code> and  <code>myVar</code> are legal.</li><li>Capital letters are fine, but be careful. Variable names are case sensitive. A <code>rose</code> is not a <code>Rose</code>. If you assign the string \"Floribundas\" to the variable <code>rose</code>, and then ask JavaScript for the value assigned to <code>Rose</code>, you'll come up empty.</li></ul>";
+
+    document.getElementById("Output").innerHTML = html;
+    document.getElementById("Output").style.textAlign = "start"
+    
 }
 // camelCase
 document.getElementById("camelCase").onclick = function () {
-    document.getElementById("Original").innerText = "Example of CamelCase"
-    document.getElementById("Output").innerText = "thisIsCamelCase"
+    
+    document.getElementById("Original").innerHTML = "Example of camelCase"
+    let camel = "<ul style='list-style: none;' ><li><code>User</code></li><li><code>userResponse</code></li><li><code>userResponseTime</code></li><li><code>userResponseTimeLimit</code></li></ul>";
+    document.getElementById("Output").innerHTML = camel;
+    document.getElementById("Output").style.textAlign = "center"
 }
 // sum of two numnbers
 document.getElementById("sum-of-num").onclick = function () {
